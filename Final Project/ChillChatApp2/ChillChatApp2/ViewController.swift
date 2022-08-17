c1838aecdd7f3ed653cd441d734d5f0372074755
DanielleAkojenu <110996707+DanielleAkojenu@users.noreply.github.com>
2022-08-17 16:41:10 -0400

    Add files via upload

---
+++ Final Project/ChillChatApp2/ChillChatApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChillChatApp2
+//
+//  Created by Scholar on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
